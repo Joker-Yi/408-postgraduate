12a4c6d1072792b879f66f5d1eab1b79ad3ed4e6
Yee <735734221@qq.com>
2021-09-12 18:03:30 +0800

    链队列（不带头结点）&基本操作

---
+++ Data structure/queue/3.linkQueue_withoutHead.cpp
@@ -0,0 +1,65 @@
+//
+// Created by Yee on 2021/9/12.
+//
+
+// 链队列 （不带头结点）
+
+#include <stdlib.h>
+// 声明一个结构体 代表链队列里的结点
+typedef struct LinkNode {
+    int data;
+    struct LinkNode *next;
+} LinkNode;
+
+// 声明一个结构体 代表该链队列(头结点)
+typedef struct {
+    LinkNode *front, *rear;
+} LinkQueue;
+
+// 初始化队列 不带头结点
+void initQueue (LinkQueue &Q) {
+    Q.front = NULL;
+    Q.rear = NULL;
+}
+
+// 判断队列是否为空
+bool isEmpty (LinkQueue Q) {
+    return Q.front == NULL;
+    // or
+    // return Q.rear == NULL;
+}
+
+// 入队操作
+void enQueue (LinkQueue &Q, int x) {
+    LinkNode *newNode = (LinkNode *) malloc(sizeof (LinkNode));
+    newNode->data = x;
+    newNode->next = NULL;
+    if (Q.front == NULL) {
+        Q.front = newNode;
+        Q.rear = newNode;
+    } else {
+        Q.rear->next = newNode; // 新结点插入至rear结点之后
+        Q.rear = newNode;
+    }
+}
+
+// 出队操作
+bool deQueue (LinkQueue &Q, int &x) {
+    if (Q.front == NULL) {
+        return false; // 空队不允许出队
+    }
+    LinkNode *p = Q.front;
+    x = p->data;
+    Q.front = p->next; // 修改头结点的next指针指向
+    if (Q.rear == p) { // 队中最后一元素出栈
+        Q.rear = Q.front = NULL; // 重制为空队
+    }
+    free(p);
+    return true;
+}
+
+int main () {
+    LinkQueue Q;
+    initQueue(Q);
+    return 0;
+}
